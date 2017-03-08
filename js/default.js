c4c52159c26877bb7641b1bafc706d0500720dbd
Pocki <lukas-pockrandt@web.de>
2017-03-08 19:57:40 +0100

    q123asd

--- js/default.js
+++ js/default.js
@@ -22,17 +22,42 @@ function loadJSON(path, success, error)
     xhr.send(null);
 }
 
-var products;
-var path = document.getElementsByTagName("script")[0].src;
-path = path.slice(0,-10) + "json/products.json";
-loadJSON(path, function(data){products = data}, function(xhr){console.log(xhr)});
-
-window.onload = function () { 
-	document.getElementById("title").innerHTML = "hahahahahaha";
-	document.getElementById("image").src = "images/salami.png";
+function showCart(products)
+{
+    document.getElementById("container").innerHTML = "";
 	for(var i = 0; i < products.length; i++)
 	{
 		addPizza(i, products[i].name, products[i].image, products[i].price);
 	}
+}
+
+function start(products)
+{
+	document.getElementById("container").innerHTML = "";
+	for(var i = 0; i < products.length; i++)
+	{
+		addPizza(i, products[i].name, products[i].image, products[i].price);
+	}
+}
+
+var products;
+var cart;
+var startPath = document.getElementsByTagName("script")[0].src;
+var cartPath = document.getElementsByTagName("script")[0].src;
+startPath = startPath.slice(0,-10) + "json/products.json";
+cartPath = cartPath.slice(0,-10) + "json/cart.json";
+loadJSON(startPath, function(data){products = data}, function(xhr){console.log(xhr)});
+loadJSON(cartPath, function(data){cart = data}, function(xhr){console.log(xhr)});
+
+window.onload = function () { 
+	document.getElementById("title").innerHTML = "hahahahahaha";
+	document.getElementById("image").src = "images/salami.png";
+	start(products);
+	document.getElementById("showCart").addEventListener("click", function() {
+    showCart(cart);
+});
+	document.getElementById("start").addEventListener("click", function() {
+    start(products);
+});
 	
 }
