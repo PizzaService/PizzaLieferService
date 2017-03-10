50c550e72fbe75fe971eac50cb4c95009dffb240
Lars Bauhofer <lars.bauhofer@gmail.com>
2017-03-10 12:27:33 +0100

    added cart

--- js/default.js
+++ js/default.js
@@ -1,17 +1,58 @@
+var cart = [];
+var products = [];
+var cartCnt = 0;
+
 function addPizza(id, name, imgPath, price)
 {
-	document.getElementById("container").innerHTML +="<br /><div id=\""+id+"\"><p>"+name+"</p><br /><img width=\"200px\" height=\"200px\" src=\""+imgPath+"\" /><br /><p>preis: "+price+"</p><br /><button>Pizza hinzuf&uuml;gen</button></div>";
-}
-function addPizzaCart(id, name, price)
-{
-	document.getElementById("container").innerHTML +="<div id=\""+id+"\">"+name+"; Preis: "+price+"<button id=\"button"+id+"\">&#10006;</button></div>";
-	
+	document.getElementById("container").innerHTML +="<br /><div id=\""+id+"\"><p>"+name+"</p><br /><img width=\"200px\" height=\"200px\" src=\""+imgPath+"\" /><br /><p>preis: "+price+"</p><br /><button onclick=\"addToCart("+id+")\">Pizza hinzuf&uuml;gen</button></div>";
 }
 
-function bla()
+function addPizzaToCart(artNr, amount)
 {
-	alert("jo");
+    for (var i = 0; i < products.length; i++)
+    {
+        if (products[i].artNr == artNr)
+        {
+            document.getElementById("container").innerHTML += "<div id=\"" + products[i].artNr + "\">" + products[i].name + ";\t Preis: " + products[i].price + ";\t Anzahl: " + amount + "<button onclick=\"deleteFromCart(" + products[i].artNr + ")\">&#10006;</button></div>";
+            return;
+        }    
+    }
+}
 
+function addToCart(artNr) 
+{
+    if (cart[artNr])
+        cart[artNr]++;
+    else
+        cart[artNr] = 1;
+    cartCnt++;
+    updateCartCount();
+}
+
+function deleteFromCart(artNr) {
+    if (--cart[artNr] == 0)
+        cart[artNr] = null;
+    loadCart();
+    cartCnt--;
+    updateCartCount();
+}
+
+function clearCart() {
+    cart = [];
+    loadCart();
+    cartCnt = 0;
+    updateCartCount();
+}
+
+function updateCartCount() {
+    if(cartCnt != 0)
+        document.getElementById("btnCart").innerText = document.getElementById("btnCart").innerText.slice(0, 9) + " " + cartCnt;
+    else
+        document.getElementById("btnCart").innerText = document.getElementById("btnCart").innerText.slice(0, 9)
+}
+
+function clearCartCount() {
+    document.getElementById("btnCart").innerText = document.getElementById("btnCart").innerText.slice(0,9);
 }
 
 function loadJSON(path, success, error)
@@ -26,53 +67,58 @@ function loadJSON(path, success, error)
 				if (success)
 					success(JSON.parse(xhr.responseText));
 			} else {
-				if (error)
-					error(xhr);
+			    if (error)
+			        error(xhr);
 			}
     };
     xhr.send(null);
 }
 
-function showCart(products)
+function loadCart()
 {
     document.getElementById("container").innerHTML = "";
-	for(var i = 0; i < products.length; i++)
-	{
-		addPizzaCart(i, products[i].name, products[i].price);
-		(function (i) {																		//
-			document.getElementById("button"+i).addEventListener("click", function () {		//	setzt einen listener auf den aktuellen button. funktioniert nur net so wies soll.
-				alert(i);																	// 					
-			});
-		}(i));
-		
-	}
+
+    for (var i = 0; i < cart.length; i++) {
+        if(cart[i])
+        {
+            addPizzaToCart(i, cart[i]);
+        }
+    }
+    document.getElementById("container").innerHTML += "<button onclick=\"clearCart()\">Warenkorb leeren</button>"
 }
 
-function start(products)
+function loadMain()
 {
 	document.getElementById("container").innerHTML = "";
 	for(var i = 0; i < products.length; i++)
 	{
-		addPizza(i, products[i].name, products[i].image, products[i].price);
+		addPizza(products[i].artNr, products[i].name, products[i].image, products[i].price);
 	}
 }
 
-var products;
-var cart;
-var startPath = document.getElementsByTagName("script")[0].src;
-var cartPath = document.getElementsByTagName("script")[0].src;
-startPath = startPath.slice(0,-10) + "json/products.json";
-cartPath = cartPath.slice(0,-10) + "json/cart.json";
-loadJSON(startPath, function(data){products = data}, function(xhr){console.log(xhr)});
-loadJSON(cartPath, function(data){cart = data}, function(xhr){console.log(xhr)});
+window.onload = function () {
+    var startPath = document.getElementsByTagName("script")[0].src;
+    var cartPath = document.getElementsByTagName("script")[0].src;
+    startPath = startPath.slice(0, -10) + "json/products.json";
+    cartPath = cartPath.slice(0, -10) + "json/cart.json";
 
-window.onload = function () { 
-	start(products);
-	document.getElementById("showCart").addEventListener("click", function() {
-    showCart(cart);
-});
-	document.getElementById("showStart").addEventListener("click", function() {
-    start(products);
-});
-	
+    loadJSON(startPath, function (data) { products = data; loadMain(); }, function (xhr) { console.log(xhr) });
+    
+    if (localStorage.getItem("CartPizzaService"))
+    {
+        cart = JSON.parse(localStorage.getItem("CartPizzaService"));
+        for (var i = 0; i < cart.length; i++)
+        {
+            if (cart[i])
+                cartCnt += cart[i];
+        }
+        updateCartCount();
+    }
+}
+
+window.onbeforeunload = function () {
+    if (cart.length != 0)
+    {
+        window.localStorage.setItem("CartPizzaService", JSON.stringify(cart));
+    }
 }
