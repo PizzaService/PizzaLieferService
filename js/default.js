fb89407ffd9ca9d1cb9f781c8fa9d9a3b68b1844
Pocki <lukas-pockrandt@web.de>
2017-03-13 16:15:51 +0100

    made with love

--- js/default.js
+++ js/default.js
@@ -1,22 +1,98 @@
 var cart = [];
 var products = [];
 var cartCnt = 0;
+var expanded = false;
 
 function addPizza(id, name, imgPath, price)
 {
-	document.getElementById("container").innerHTML +="<br /><div id=\""+id+"\"><p>"+name+"</p><br /><img width=\"200px\" height=\"200px\" src=\""+imgPath+"\" /><br /><p>preis: "+price+"</p><br /><button onclick=\"addToCart("+id+")\">Pizza hinzuf&uuml;gen</button></div>";
+	/*document.getElementById("container").innerHTML +="<br /><div id=\""+id+"\"><p>"+name+"</p><br /><img width=\"200px\" height=\"200px\" src=\""+imgPath+"\" /><br /><p>preis: "+price+"</p><br /><button onclick=\"addToCart("+id+")\">Pizza hinzuf&uuml;gen</button></div>";*/
+	var pizzaDiv = document.createElement("div");
+	pizzaDiv.id = id;
+	pizzaDiv.innerHTML = name;
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+
+	var elem = document.createElement("img");
+	elem.setAttribute("height", "200");
+	elem.setAttribute("width", "200");
+	elem.src = imgPath;
+	pizzaDiv.appendChild(elem);
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+
+	var priceText = document.createTextNode("Preis: " + price);
+	pizzaDiv.appendChild(priceText);
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+
+	var btn = document.createElement("button");
+	btn.setAttribute('onclick',"addToCart(" + id + ")");
+	btn.innerHTML = "Pizza hinzuf&uuml;gen";
+	pizzaDiv.appendChild(btn);
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+
+	lb = document.createElement("br");
+	pizzaDiv.appendChild(lb);
+	
+	document.getElementById("container").appendChild(pizzaDiv);
 }
 
 function addPizzaToCart(artNr, amount)
 {
     for (var i = 0; i < products.length; i++)
     {
-        if (products[i].artNr == artNr)
+        /*if (products[i].artNr == artNr)
         {
             document.getElementById("container").innerHTML += "<div id=\"" + products[i].artNr + "\">" + products[i].name + ";\t Preis: " + products[i].price + ";\t Anzahl: " + amount + "<button onclick=\"deleteFromCart(" + products[i].artNr + ")\">&#10006;</button></div>";
             return;
-        }    
+        }
+		*/if (products[i].artNr == artNr)
+		{
+			var div = document.createElement("div");
+			div.id = products[i].artNr;
+			div.innerHTML = products[i].name;
+
+			var priceText = document.createTextNode("; Preis: " + products[i].price);
+			div.appendChild(priceText);
+
+			var amountText = document.createTextNode("; Anzahl: " + amount);
+			div.appendChild(amountText);
+
+			var btn = document.createElement("button");
+			btn.setAttribute('onclick',"deleteFromCart(" + products[i].artNr + ")");
+			btn.innerHTML = '&#10006';
+			div.appendChild(btn);
+
+			document.getElementById("container").appendChild(div);
+
+			return;
+		}    
     }
+}
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
 }
 
 function addToCart(artNr) 
@@ -45,14 +121,15 @@ function clearCart() {
 }
 
 function updateCartCount() {
-    if(cartCnt != 0)
-        document.getElementById("btnCart").innerText = document.getElementById("btnCart").innerText.slice(0, 9) + " " + cartCnt;
-    else
-        document.getElementById("btnCart").innerText = document.getElementById("btnCart").innerText.slice(0, 9)
+    if(cartCnt != 0) {
+        document.getElementById("btnCart").innerHTML = "Warenkorb " + cartCnt;
+    } else {
+        document.getElementById("btnCart").innerHTML = "Warenkorb";
+    }
 }
 
 function clearCartCount() {
-    document.getElementById("btnCart").innerText = document.getElementById("btnCart").innerText.slice(0,9);
+    document.getElementById("btnCart").innerHTML = document.getElementById("btnCart").innerHTML.slice(0,9);
 }
 
 function loadJSON(path, success, error)
