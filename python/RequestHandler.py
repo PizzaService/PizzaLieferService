2430c3c57c3ec21d9492132c89f86382119d6545
Totrock <josef.probst7@googlemail.com>
2017-04-07 12:10:50 +0200

    Add files via upload
    Done

--- python/RequestHandler.py
+++ python/RequestHandler.py
@@ -1,13 +1,38 @@
 import urllib
 import http.server
+import http.cookies
 import json
+import datetime
+import random
+
 
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
 
+    JSONFILE = "data.json"
+    MAX_CLIENTS = 50
+    COOKIE_LIFESPAN = 900   #seconds
+    sendcookies = 1         #true
+    onlyupdate = 0          #false
+    id_of_cookie_to_update = 0
+    Cookie_Box = {}
+    list_of_Orders = {}
 
     def do_GET(self):#https://opensource.apple.com/source/python/python-3/python/Lib/SimpleHTTPServer.py
         """Serve a GET request."""
+        self.sendcookies = 1
+        self.onlyupdate = 0
+        self.delete_old_cookies()
 
+        if self.headers["cookie"]:
+             if self.Cookie_Box.__contains__(int(str(self.headers).split("Cookie: id=")[1].splitlines()[0])):
+                self.onlyupdate = 1
+                self.sendcookies = 0
+                self.id_of_cookie_to_update = int(str(self.headers).split("Cookie: id=")[1].splitlines()[0])
+        else:
+            if len(self.Cookie_Box) >= self.MAX_CLIENTS:
+                self.sendcookies = 0
+                self.send_error(503)
+                return
 
 
         f = self.send_head()
@@ -18,23 +43,50 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):#https://stackoverflow.com/questions/2121481/python3-http-server-post-example
         length = int(self.headers['Content-Length'])
         post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-        # You now have a dictionary of the post data
-        print(post_data)
+        # dictionary of the post data
 
+        self.list_of_Orders[len(self.list_of_Orders)] = post_data
 
-
-
-        #https://stackoverflow.com/questions/7100125/storing-python-dictionaries
-        with open('data.json', 'w') as jsontemp:
-            json.dump(post_data, jsontemp, indent=3)
-
-        with open('data.json', 'r') as jsontemp:
-            datadict = json.load(jsontemp)
-
-        print(datadict["name"])
+        self.write_list_to_json(self.list_of_Orders,"data.json")
 
 
         f = self.send_head()
         if f:
             self.copyfile(f, self.wfile)
             f.close()
+
+
+    def write_list_to_json(self,dict,jsonfile):
+        # https://stackoverflow.com/questions/7100125/storing-python-dictionaries
+        with open(jsonfile, 'w') as jsontemp:
+            json.dump(dict, jsontemp, indent=3)
+
+
+    def read_json_to_dict(jsonfile):
+        with open(jsonfile, 'r') as jsontemp:
+            datadict = json.load(jsontemp)
+        return datadict
+
+
+    def end_headers(self):#https://stackoverflow.com/questions/12499171/can-i-set-a-header-with-pythons-simplehttpserver
+        self.send_cookies_header()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_cookies_header(self):
+        if self.sendcookies:
+            #print(self.Cookie_Box)
+            id = random.randint(1,1000000000)
+            exp = datetime.datetime.now()+datetime.timedelta(seconds=self.COOKIE_LIFESPAN)
+            self.Cookie_Box[id] = exp
+            self.send_header("Set-Cookie", "id=%s; Max-Age=%s;path=/" % (id,self.COOKIE_LIFESPAN))
+
+        if self.onlyupdate:
+            #print(self.Cookie_Box)
+            self.send_header("Set-Cookie", "id=%s; Max-Age=%s;path=/" % (self.id_of_cookie_to_update, self.COOKIE_LIFESPAN))
+            exp = datetime.datetime.now() + datetime.timedelta(seconds=self.COOKIE_LIFESPAN)
+            self.Cookie_Box[self.id_of_cookie_to_update] = exp
+
+    def delete_old_cookies(self):
+        for key, value in self.Cookie_Box.items():
+            if datetime.datetime.now()>value:
+                del self.Cookie_Box[key]
